ab5759f819fecb4d3f87f3d646b0f044718b1817
cdub-616 <cdub616@aol.com>
2022-10-13 16:44:35 -0700

    added login_error page and redirect back to login page

--- MANYC/src/main/webapp/login.js
+++ MANYC/src/main/webapp/login.js
@@ -1,6 +1,8 @@
 /** Creates login page
  *  Ashar, Nick
  *  version 1.0.0  9/27/2022
+ *          1.1.0  10/5/2022 condition valid -> grid page
+ *          1.2.0  10/12/2022 condition invalid -> error page
  *
  *  validate() verifies username and password are correct */
  
@@ -11,10 +13,9 @@
 	if (username =="username" && password=="password")
 		{
 		window.location.href = "dynamic_grid.html";
-		alert("login succesfully");
 		}
 	else 
 		{
-		alert("login failed")
+		window.location.href = "login_error.html";
 		}
 }
