ccb6ceda34b4733c8ad930ede8eea9c08d17ec3a
cdub-616 <cdub616@aol.com>
2022-10-19 18:18:29 -0700

    cleaned up folders and files and added database

---
+++ MANYC/frontend/login.js
@@ -0,0 +1,21 @@
+/** Creates login page
+ *  Ashar, Nick, Yahir
+ *  version 1.0.0  9/27/2022
+ *          1.1.0  10/5/2022 condition valid -> grid page
+ *          1.2.0  10/12/2022 condition invalid -> error page
+ *
+ *  validate() verifies username and password are correct */
+ 
+ function validate()
+{
+	var username=document.getElementById("username").value;
+	var password=document.getElementById("password").value;
+	if (username =="username" && password=="password")
+		{
+		window.location.href = "grid.html";
+		}
+	else 
+		{
+		window.location.href = "login_error.html";
+		}
+}
