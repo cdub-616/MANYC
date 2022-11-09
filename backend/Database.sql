69d8c9ea296110b9a9a2eca8c907c6e229207640
cdub-616 <cdub616@aol.com>
2022-11-08 17:52:03 -0800

    changes to login.js

--- backend/Database.sql
+++ backend/Database.sql
@@ -3,8 +3,6 @@ except is a template file which goes in mysql workbench*/
 CREATE DATABASE employees;
 USE employees;
 CREATE TABLE employee_changes (
-	id INT NOT NULL AUTO_INCREMENT,
-    PRIMARY KEY (id),
-    ids VARCHAR(200) NOT NULL,
+	ids VARCHAR(200) NOT NULL,
     status VARCHAR(10) NOT NULL
 );
