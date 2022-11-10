ae5c717629d00dc18f9a3e3a05c90276a79f11e5
cdub-616 <cdub616@aol.com>
2022-11-09 16:21:31 -0800

    changes to index.js

--- frontend/login.js
+++ frontend/login.js
@@ -3,10 +3,14 @@
  *  version 1.0.0  9/27/2022
  *          1.1.0  10/5/2022 condition valid -> grid page
  *          1.2.0  10/12/2022 condition invalid -> error page
+ *			1.3.0  11/7/2022 prevent those in the database from logging 
+ *                           in/filter system
  *
  *  validate() verifies username and password are correct */
  function validate()
 {
+	//Saving for back up
+	
 	var username=document.getElementById("username").value;
 	var password=document.getElementById("password").value;
 	if (username =="username" && password=="password")
@@ -17,4 +21,34 @@
 		{
 		window.location.href = "login_error.html";
 		}
+	
+	/*const mysql = require("mysql");
+	var db = mysql.createConnection({
+		host:'localhost',
+		user:'root',
+		password:'password',
+		database:'employees'
+	});
+	var username=document.getElementById("username").value;
+	var password=document.getElementById("password").value;
+	
+	switch (username) {
+		case 'ids': 
+			window.location.href = "login_error.html";
+			break;
+		case 'username': 
+			window.location.href = "grid.html";
+			break;
+		default: 
+			window.location.href = "login_error.html";
+		
+	}
+		
+	switch (password) {
+		case 'password':
+			window.location.href = "grid.html";
+			break;
+		default:
+			window.location.href = "login_error.html";
+	}*/	
 }
