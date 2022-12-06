659dca46b8e600928868d6e42832da5db229ef07
cdub-616 <cdub616@aol.com>
2022-12-05 16:57:21 -0800

    right click grid pop-up

--- frontend/gridmenu.js
+++ frontend/gridmenu.js
@@ -1,42 +1,43 @@
 /**  Creates a pop-up on grid page with legend and buttons
- *   Yahir
+ *   Yahir, Chris
  *   version 1.0.0  11/9/2022
- *
- */
+ *           1.0.1  12/2/2022  pop-up activates by clicking screen
+ *           1.0.2  12/5/2022  pop-up activates by right click*/
 
 const openModalButtons = document.querySelectorAll('[data-modal-target]')
 const closeModalButtons = document.querySelectorAll('[data-close-button]')
 const overlay = document.getElementById('overlay')
 
 openModalButtons.forEach(button => {
-  button.addEventListener('click', () => {
-    const modal = document.querySelector(button.dataset.modalTarget)
-    openModal(modal)
-  })
+    button.addEventListener('contextmenu', (error) => {
+        error.preventDefault();
+        const modal = document.querySelector(button.dataset.modalTarget)
+        openModal(modal)
+    })
 })
 
-overlay.addEventListener('click', () => {
-  const modals = document.querySelectorAll('.modal.active')
-  modals.forEach(modal => {
-    closeModal(modal)
-  })
+overlay.addEventListener('click', (error) => {
+    const modals = document.querySelectorAll('.modal.active')
+    modals.forEach(modal => {
+        closeModal(modal)
+    })
 })
 
 closeModalButtons.forEach(button => {
-  button.addEventListener('click', () => {
-    const modal = button.closest('.modal')
-    closeModal(modal)
-  })
+    button.addEventListener('click', (error) => {
+        const modal = button.closest('.modal')
+        closeModal(modal)
+    })
 })
 
 function openModal(modal) {
-  if (modal == null) return
-  modal.classList.add('active')
-  overlay.classList.add('active')
+    if (modal == null) return
+    modal.classList.add('active')
+    overlay.classList.add('active')
 }
 
 function closeModal(modal) {
-  if (modal == null) return
-  modal.classList.remove('active')
-  overlay.classList.remove('active')
+    if (modal == null) return
+    modal.classList.remove('active')
+    overlay.classList.remove('active')
 }
